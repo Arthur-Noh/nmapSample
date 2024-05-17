855cacbd143f5e50221c0f1de5401e041cd67721
Arthur-Noh <dkagod97@gmail.com>
2024-05-17 11:04:58 +0900

    resolve: conflict

--- App.tsx
+++ App.tsx
@@ -13,9 +13,9 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  useColorScheme, useWindowDimensions,
+  View
+} from "react-native";
 
 import {
   Colors,
@@ -24,6 +24,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { NaverMapView } from "@mj-studio/react-native-naver-map";
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -56,6 +57,7 @@ function Section({children, title}: SectionProps): JSX.Element {
 }
 
 function App(): JSX.Element {
+  const { width, height } = useWindowDimensions();
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -64,34 +66,9 @@ function App(): JSX.Element {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View style={{width, height, backgroundColor: 'red'}}>
+        <NaverMapView style={{flex: 1}} />
+      </View>
     </SafeAreaView>
   );
 }
